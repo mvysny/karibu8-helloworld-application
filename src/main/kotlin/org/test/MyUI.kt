6e8e3f4e2e7a311aa82c5f16919d33bcd5739301
Martin Vysny <mavi@vaadin.com>
2017-06-04 09:56:24 +0300

    Added the MyUI.kt

---
+++ src/main/kotlin/org/test/MyUI.kt
@@ -0,0 +1,45 @@
+package org.test
+
+import com.github.vok.karibudsl.button
+import com.github.vok.karibudsl.label
+import com.github.vok.karibudsl.textField
+import com.github.vok.karibudsl.verticalLayout
+import javax.servlet.annotation.WebServlet
+
+import com.vaadin.annotations.Theme
+import com.vaadin.annotations.VaadinServletConfiguration
+import com.vaadin.server.VaadinRequest
+import com.vaadin.server.VaadinServlet
+import com.vaadin.ui.Button
+import com.vaadin.ui.Label
+import com.vaadin.ui.TextField
+import com.vaadin.ui.UI
+import com.vaadin.ui.VerticalLayout
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window
+ * (or tab) or some part of a html page where a Vaadin application is embedded.
+ *
+ * The UI is initialized using [init]. This method is intended to be
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("valo") // @todo use mytheme
+class MyUI : UI() {
+
+    private lateinit var layout: VerticalLayout
+
+    @Override
+    override fun init(vaadinRequest: VaadinRequest) {
+        layout = verticalLayout {
+            val name = textField {
+                caption = "Type your name here:"
+            }
+            button("Click Me", { layout.label("Thanks ${name.value}, it works!") })
+        }
+    }
+
+}
+
+@WebServlet(urlPatterns = arrayOf("/*"), name = "MyUIServlet", asyncSupported = true)
+@VaadinServletConfiguration(ui = MyUI::class, productionMode = false)
+class MyUIServlet : VaadinServlet()
