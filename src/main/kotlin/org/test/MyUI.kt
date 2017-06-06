8df9fd53d267ad783ce1764d5a8c36ff2de6a706
Martin Vysny <mavi@vaadin.com>
2017-06-06 16:48:12 +0300

    minor

--- src/main/kotlin/org/test/MyUI.kt
+++ src/main/kotlin/org/test/MyUI.kt
@@ -10,9 +10,6 @@ import com.vaadin.annotations.Theme
 import com.vaadin.annotations.VaadinServletConfiguration
 import com.vaadin.server.VaadinRequest
 import com.vaadin.server.VaadinServlet
-import com.vaadin.ui.Button
-import com.vaadin.ui.Label
-import com.vaadin.ui.TextField
 import com.vaadin.ui.UI
 import com.vaadin.ui.VerticalLayout
 
