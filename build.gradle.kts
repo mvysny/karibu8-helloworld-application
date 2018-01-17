6ac7affa5650e66e3e01f13646ec2faa9ae72d96
Martin Vysny <martin.vysny@ericsson.com>
2018-01-17 10:00:51 +0200

    Ported build.gradle to Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.2.10"
+    // need to use Gretty here because of https://github.com/johndevs/gradle-vaadin-plugin/issues/317
+    id("org.akhikhl.gretty") version "2.0.0"
+    id("com.devsoap.plugin.vaadin") version "1.2.6"
+}
+
+defaultTasks("clean", "build")
+
+repositories {
+    mavenCentral()
+    jcenter()
+    maven("https://dl.bintray.com/mvysny/vaadin-on-kotlin")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+vaadin {
+    version = "8.2.1"
+}
+
+gretty {
+    contextPath = "/"
+}
+
+dependencies {
+    // Karibu-DSL dependency
+    compile("com.github.vok.karibudsl:karibu-dsl-v8:0.2.17")
+
+    // include proper kotlin version
+    compile(kotlin("stdlib-jre8"))
+
+    // test support
+    testCompile("com.github.vok.karibudsl:karibu-testing-v8:0.2.17")
+    testCompile("junit:junit:4.12")
+    testCompile(kotlin("test"))
+
+    // workaround until https://youtrack.jetbrains.com/issue/IDEA-178071 is fixed
+    compile("com.vaadin:vaadin-themes:${vaadin.version}")
+    compile("com.vaadin:vaadin-client-compiled:${vaadin.version}")
+}
