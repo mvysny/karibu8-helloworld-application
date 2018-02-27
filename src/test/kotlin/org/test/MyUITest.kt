f5e3421448392f3456847a2aac05121df9e1368a
Martin Vysny <mavi@vaadin.com>
2018-02-27 19:41:33 +0200

    added comments to MyUITest

--- src/test/kotlin/org/test/MyUITest.kt
+++ src/test/kotlin/org/test/MyUITest.kt
@@ -9,15 +9,26 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.expect
 
+/**
+ * Tests the UI. Uses the [Serverless testing approach](https://github.com/mvysny/karibu-testing).
+ */
 class MyUITest : DynaTest({
     beforeEach { MockVaadin.setup({ MyUI() }) }
 
     test("simple UI test") {
+        // MockVaadin.setup() prepared the UI for us; we can now read components from it.
         val layout = UI.getCurrent().content as VerticalLayout
+
+        // // simulate a text entry as if entered by the user
         _get<TextField> { caption = "Type your name here:" }.value = "Baron Vladimir Harkonnen"
+
+        // simulate a button click as if clicked by the user
         _get<Button> { caption = "Click Me" }._click()
+
+        // check that the layout now has three components
         expect(3) { layout.componentCount }
-        expect("Thanks Baron Vladimir Harkonnen, it works!") { (layout.last() as Label).value }
+
+        // verify that there is a single Label and assert on its value
         expect("Thanks Baron Vladimir Harkonnen, it works!") { _get<Label>().value }
     }
 })
