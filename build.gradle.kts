87015dd96c543073113931aeb709e345684cc0a8
Martin Vysny <mavi@vaadin.com>
2018-03-26 08:56:56 +0300

    v bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ tasks.withType<Test> {
 
 dependencies {
     // Karibu-DSL dependency
-    compile("com.github.vok.karibudsl:karibu-dsl-v8:0.3.8")
+    compile("com.github.vok.karibudsl:karibu-dsl-v8:0.3.9")
 
     // include proper kotlin version
     compile(kotlin("stdlib-jdk8"))
