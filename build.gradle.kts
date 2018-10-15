bfc4ce40a76df815800df5266f2c30254815fd93
Martin Vysny <martin@vysny.me>
2018-10-15 12:01:44 +0300

    v bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 plugins {
     kotlin("jvm") version "1.2.71"
     // need to use Gretty here because of https://github.com/johndevs/gradle-vaadin-plugin/issues/317
-    id("org.gretty") version "2.1.0"
+    id("org.gretty") version "2.2.0"
     id("com.devsoap.plugin.vaadin") version "1.3.1"
 }
 
@@ -24,6 +24,7 @@ vaadin {
 
 gretty {
     contextPath = "/"
+    servletContainer = "jetty9.4"
 }
 
 tasks.withType<Test> {
