f969c9c53b37585bec1d879b9d1787bc620b350f
Martin Vysny <martin@vysny.me>
2019-03-06 12:20:01 +0200

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ val staging by configurations.creating
 
 dependencies {
     // Karibu-DSL dependency
-    compile("com.github.mvysny.karibudsl:karibu-dsl-v8:0.6.1")
+    compile("com.github.mvysny.karibudsl:karibu-dsl-v8:0.6.2")
 
     // include proper kotlin version
     compile(kotlin("stdlib-jdk8"))
@@ -51,7 +51,7 @@ dependencies {
     compile("org.slf4j:jul-to-slf4j:1.7.25")
 
     // test support
-    testCompile("com.github.mvysny.kaributesting:karibu-testing-v8:1.1.3")
+    testCompile("com.github.mvysny.kaributesting:karibu-testing-v8:1.1.4")
     testCompile("com.github.mvysny.dynatest:dynatest-engine:0.15")
 
     // workaround until https://youtrack.jetbrains.com/issue/IDEA-178071 is fixed
