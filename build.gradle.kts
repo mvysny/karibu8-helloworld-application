47dee7262d87bd681fcb31a55c3d929ddf9fb5cf
Martin Vysny <martin@vysny.me>
2019-03-20 17:01:27 +0200

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ tasks.withType<KotlinCompile> {
 }
 
 vaadin {
-    version = "8.7.0"
+    version = "8.7.1"
 }
 
 gretty {
@@ -39,7 +39,7 @@ val staging by configurations.creating
 
 dependencies {
     // Karibu-DSL dependency
-    compile("com.github.mvysny.karibudsl:karibu-dsl-v8:0.6.2")
+    compile("com.github.mvysny.karibudsl:karibu-dsl-v8:0.6.3")
 
     // include proper kotlin version
     compile(kotlin("stdlib-jdk8"))
