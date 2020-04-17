875969124c8c63d88e5a40e190211fa638d304a1
Martin Vysny <martin@vysny.me>
2020-04-17 22:59:48 +0300

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
-    kotlin("jvm") version "1.3.71"
+    kotlin("jvm") version "1.3.72"
     // need to use Gretty here because of https://github.com/johndevs/gradle-vaadin-plugin/issues/317
     id("org.gretty") version "3.0.1"
     id("com.devsoap.plugin.vaadin") version "2.0.0.beta2"
@@ -39,7 +39,7 @@ val staging by configurations.creating
 
 dependencies {
     // Karibu-DSL dependency
-    compile("com.github.mvysny.karibudsl:karibu-dsl-v8:0.7.5")
+    compile("com.github.mvysny.karibudsl:karibu-dsl-v8:1.0.0")
 
     // include proper kotlin version
     compile(kotlin("stdlib-jdk8"))
